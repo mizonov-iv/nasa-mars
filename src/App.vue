7e1ede0f79c3e0097099e5821cb27b87e1452477
mizonov-iv <mizonov.iv@gmail.com>
2023-12-12 21:43:17 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${this.selectedRover}/photos?sol=${+this.selectedSol}&camera=${this.selectedCamera.abbreviation}&api_key=tGp4b8kAlchB0pvLpHNeWHVCKRyMpPPy1pQ0jhlK`)
+
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
