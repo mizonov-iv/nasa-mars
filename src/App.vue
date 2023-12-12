fa086d107caf1595fc58f30bc20966eb60ce6f9e
mizonov-iv <mizonov.iv@gmail.com>
2023-12-12 23:03:06 +0500

    rover choose logic

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,151 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
-
-axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${this.selectedRover}/photos?sol=${+this.selectedSol}&camera=${this.selectedCamera.abbreviation}&api_key=tGp4b8kAlchB0pvLpHNeWHVCKRyMpPPy1pQ0jhlK`)
-
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <select v-model="selectedRover">
+      <option disabled value="">Выберите ровер</option>
+      <option
+          v-for="rover in roversList"
+          :key="rover.id"
+          :value="rover.name"
+      >
+        {{rover.name}}
+      </option>
+    </select>
+
+    <select>
+      <option disabled value="">Выберите камеру</option>
+      <option v-for="camera in cameras" :key="camera" :value="camera.name">{{camera.name}}</option>
+    </select>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import axios from 'axios'
+import {computed, onMounted, ref} from "vue";
+
+const API_KEY = 'RRXijm1h3BtuMsaXOIKawe81ERYXsWcGVemIW8FJ'
+const roversList = [
+  {
+    id: 0,
+    name: "Curiosity"
+  },
+  {
+    id: 1,
+    name: "Opportunity"
+  },
+  {
+    id: 2,
+    name: "Spirit"
+  }
+]
+const camerasList = [
+  {
+    name: "Front Hazard Avoidance Camera",
+    abbreviation: "FHAZ"
+  },
+  {
+    name: "RHAZ",
+    abbreviation: "Rear Hazard Avoidance Camera"
+  },
+  {
+    name: "MAST",
+    abbreviation: "Mast Camera"
+  },
+  {
+    name: "CHEMCAM",
+    abbreviation: "Chemistry and Camera Complex"
+  },
+  {
+    name: "MAHLI",
+    abbreviation: "Mars Hand Lens Imager"
+  },
+  {
+    name: "MARDI",
+    abbreviation: "Mars Descent Imager"
+  },
+  {
+    name: "NAVCAM",
+    abbreviation: "Navigation Camera"
+  },
+  {
+    name: "PANCAM",
+    abbreviation: "Panoramic Camera"
+  },
+  {
+    name: "MINITES",
+    abbreviation: "Miniature Thermal Emission Spectrometer (Mini-TES)"
+  },
+]
+
+const curiosityCameras = [
+  {
+    name: "Front Hazard Avoidance Camera",
+    abbreviation: "FHAZ"
+  },
+  {
+    name: "Rear Hazard Avoidance Camera",
+    abbreviation: "RHAZ"
+  },
+  {
+    name: "Mast Camera",
+    abbreviation: "MAST"
+  },
+  {
+    name: "Chemistry and Camera Complex",
+    abbreviation: "CHEMCAM"
+  },
+  {
+    name: "Mars Hand Lens Imager",
+    abbreviation: "MAHLI"
+  },
+  {
+    name: "Mars Descent Imager",
+    abbreviation: "MARDI"
+  },
+  {
+    name: "Navigation Camera",
+    abbreviation: "NAVCAM"
+  },
+]
+const opportunityAndSpiritCameras = [
+  {
+    name: "Front Hazard Avoidance Camera",
+    abbreviation: "FHAZ"
+  },
+  {
+    name: "RHAZ",
+    abbreviation: "Rear Hazard Avoidance Camera"
+  },
+  {
+    name: "NAVCAM",
+    abbreviation: "Navigation Camera"
+  },
+  {
+    name: "PANCAM",
+    abbreviation: "Panoramic Camera"
+  },
+  {
+    name: "MINITES",
+    abbreviation: "Miniature Thermal Emission Spectrometer (Mini-TES)"
+  },
+]
+
+const resp = ref()
+
+const selectedRover = ref("")
+
+const cameras = computed(() => {
+  if(selectedRover.value) {
+    if(selectedRover.value === "Curiosity") {
+      return curiosityCameras
+    } else return opportunityAndSpiritCameras
+  }
+})
+
+onMounted(() => {
+  axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_KEY}`)
+      .then(response => {
+        resp.value = response.data
+        console.log(response)
+      })
+})
+</script>
