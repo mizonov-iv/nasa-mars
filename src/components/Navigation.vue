e4ab3439e2f9b7037af5851f399ffa1db7b5a2ab
mizonov <mizonov.iv@gmail.com>
2024-01-10 17:29:55 +0500

    hero bg added

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,14 +1,24 @@
 <template>
-<ul class="nav-list">
-  <li class="nav-list-item" v-for="link in links">
-    {{link}}
-  </li>
-</ul>
+  <nav class="navigation">
+    <span style="color: #FFFFFF" @click="toggleBurger">Open</span>
+    <span class="nav-close-btn" v-show="navIsOpen" @click="toggleBurger">&times;</span>
+    <ul class="nav-list" v-show="navIsOpen">
+      <li class="nav-list-item" v-for="link in links">
+        {{link}}
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup>
-
 import {ref} from "vue";
+
+const navIsOpen = ref(false)
+
+const toggleBurger = () => {
+  navIsOpen.value = !navIsOpen.value
+  console.log(navIsOpen.value)
+}
 
 const links = ref([
     "Главня",
@@ -16,7 +26,3 @@ const links = ref([
     "О полете на марс"
 ])
 </script>
-
-<style scoped>
-
-</style>
